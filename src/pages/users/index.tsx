a77b9948c0c2eef4fa5589e519e502f77a6fd85f
Tomasz Adamowicz <tomasz@bracketspace.com>
2021-12-20 11:24:43 +0100

    get usere data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,12 +3,60 @@
  */
 import { NextPage } from "next";
 
-const Users: NextPage = () => {
+/**
+ * Internal dependencies
+ */
+import type { User } from '../../types';
+
+interface UsersProps {
+	usersData: User[];
+}
+
+const Users: NextPage<UsersProps> = ({ usersData }) => {
 	return (
 		<main>
 			Users
 		</main>
 	)
 };
+
+export const getServerSideProps = async () => {
+	try {
+		// const res = await fetch('');
+		const usersData: User[] = [
+			{
+				id: 1,
+				name: 'Jon Doe',
+				favoriteColor: 'red',
+				createdAt: 'Wed Feb 05 2020 11:18:21 GMT+0000 (Greenwich Mean Time)',
+			},
+			{
+				id: 2,
+				name: 'Jane Doe',
+				favoriteColor: 'green',
+				createdAt: 'Mon Dec 20 2021 11:18:21 GMT+0000 (Greenwich Mean Time)',
+			},
+			{
+				id: 3,
+				name: 'Jon XYZ',
+				favoriteColor: 'blue',
+				createdAt: 'Tue Dec 21 2021 11:18:21 GMT+0000 (Greenwich Mean Time)',
+			}
+		];
+
+		return {
+			props: {
+				usersData,
+			}
+		}
+	} catch(e) {
+		console.log(e);
+
+		return {
+			notFound: true,
+		};
+	}
+
+}
 
 export default Users;
--- src/types/user.ts
+++ src/types/user.ts
@@ -2,5 +2,5 @@ export interface User {
 	id: number,
 	name: string,
 	favoriteColor: 'red' | 'blue' | 'green',
-	createdAt: Date,
+	createdAt: string,
 }
